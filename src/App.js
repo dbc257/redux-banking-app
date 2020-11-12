e0ca810aacc9e9361a7372416bd938e5115d67de
David Carlisle <dave_0021@yahoo.com>
2020-11-11 18:39:35 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Container, Grid, Header, Segment } from 'semantic-ui-react';
+import './App.css';
+import Account from './components/Account';
+
+function App() {
+  return (
+    <Container>
+      <Segment vertical textAlign="center">
+        <Header as="h1" textAlign="center">Smells Fargo</Header>
+        <Grid columns="2">
+          <Grid.Row>
+            <Grid.Column>
+              <Account title="Checking" id="checking"/>
+            </Grid.Column>
+            <Grid.Column>
+              <Account title="Savings" id="savings"/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </Container>
+  );
+}
+
+export default App;
